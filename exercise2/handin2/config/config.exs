6b682500208f226281ca987a3a75ba92e5996242
Andreas Wachs <andreas@wachs.dk>
2022-10-22 11:30:37 +0200

    Add IS_PLAYING FLAG

--- exercise2/handin2/config/config.exs
+++ exercise2/handin2/config/config.exs
@@ -1,15 +1,17 @@
 import Config
 
 
-random_player_name = fn ->
-  :crypto.strong_rand_bytes(8) |> Base.encode16() |> String.slice(0, 7)
+default_player_name = fn ->
+  ["alice", "bob"] |> Enum.random()
 end
 
 localhost = "localhost:4040"
 
 config :handin2,
   player_names: (System.get_env("PLAYERS") || localhost)  |> String.split(","),
-  player_name: (System.get_env("PLAYER") || random_player_name.())
+  player_name: (System.get_env("PLAYER") || default_player_name.()),
+  is_playing: System.get_env("IS_PLAYING") != nil && System.get_env("IS_PLAYING") != "true"
+
 
 config :logger, :console,
   format: "$time [$level] [$metadata] $message\n",
--- exercise2/handin2/lib/client.ex
+++ exercise2/handin2/lib/client.ex
@@ -3,7 +3,7 @@ defmodule Handin2.Client do
   use GenServer
   use TypeCheck
 
-  alias Handin2.Commitments
+  alias Handin2.{Commitments, Utils, Security}
 
   @headers [{"Content-Type", "application/json"}]
 
@@ -18,18 +18,9 @@ defmodule Handin2.Client do
 
   @spec! init(:ok) :: {:ok, any()}
   def init(:ok) do
-    state = %{
-      wins: 0,
-      losses: 0,
-      draws: 0,
-      server_cheated: 0,
-    }
+    schedule_games_if_playing()
 
-    timeout = 3
-    Logger.info("Player (client) process started. Scheduling to begin playing in #{inspect(timeout)} seconds.", player: player_name())
-    schedule_next_game(timeout)
-
-    {:ok, state}
+    {:ok, gen_new_state()}
   end
 
   #
@@ -56,10 +47,17 @@ defmodule Handin2.Client do
   # Private functions
   #
 
-  def play(state) do
-    other_player = Handin2.Players.get_player()
+  defp play(state) do
+    {Utils.get_opponent_name(), state}
+    |> send_commitment()
+    |> send_reveal()
+    |> verify_game()
+    |> tap(&schedule_next_game/0)
+  end
 
-    roll = :rand.uniform(6)
+  defp send_commitment({other_player, client_state}) do
+    roll = Utils.roll_dice()
+
     {bitstring, commitment} = Commitments.create(roll |> Integer.to_string())
 
     Logger.info("Rolls #{inspect(roll)}, generates bitstring #{inspect(bitstring |> String.slice(0, 15))}.. and commitment #{inspect(commitment) |> String.slice(0, 15)}. Sends it to opponent",
@@ -69,9 +67,30 @@ defmodule Handin2.Client do
     {_, body} = post("/commit", msg, host: other_player)
     game_id = Map.get(body, "game_id")
     server_commitment = Map.get(body, "commitment")
+
     Logger.info("Received commitment from opponent #{inspect(server_commitment) |> String.slice(0, 15)}..",
       player: player_name(), opponent: other_player)
+
     msg = %{"bitstring" => bitstring, "roll" => roll}
+
+    %{
+      other_player: other_player,
+      server_commitment: server_commitment,
+      msg: msg,
+      game_id: game_id,
+      client_state: client_state
+    }
+  end
+
+  defp send_reveal(game_state) do
+    %{
+      other_player: other_player,
+      server_commitment: server_commitment,
+      msg: msg,
+      game_id: game_id,
+      client_state: client_state
+    } = game_state
+
     Logger.info("Reveals commitment to opponent",
       player: player_name(), opponent: other_player)
     {_, body} = post("/reveal/#{game_id}", msg, host: other_player)
@@ -81,8 +100,25 @@ defmodule Handin2.Client do
     Logger.info("Received opponent bitstring #{inspect(server_bitstring) |> String.slice(0, 15)}.. and roll #{inspect(server_roll)} from server",
       player: player_name(), opponent: other_player)
 
+    Map.put(client_state, :server_bitstring, server_bitstring)
+    |> Map.put(:server_roll, server_roll)
+  end
+
+  defp verify_game(game_state) do
+    # Unpack the state
+    %{
+      server_commitment: server_commitment,
+      server_bitstring: server_bitstring,
+      server_roll: server_roll,
+      roll: roll,
+      other_player: other_player,
+      client_state: client_state,
+    } = game_state
+
+    # Convert the server received server roll to a string
     server_roll_str = server_roll |> Integer.to_string()
 
+    # Verify the opening with the previously received commitment
     verification = Commitments.verify(server_commitment, server_bitstring, server_roll_str)
 
     Logger.info("Verifies opponent commitment #{inspect(verification)}",
@@ -91,17 +127,14 @@ defmodule Handin2.Client do
     Logger.info("Game result: own:#{inspect(roll)} vs opponent:#{inspect(server_roll)}. Verdict: #{inspect(determine_winner(roll, server_roll))}",
       player: player_name(), opponent: other_player)
 
-    new_state = case verification do
-      :ok -> update_state_verified(state, roll, server_roll)
-      :error -> update_state_server_cheated(state)
+    case verification do
+      :ok -> update_state_verified(client_state, roll, server_roll)
+      :error -> update_state_server_cheated(client_state)
     end
-
-    schedule_next_game()
-    new_state
   end
 
   defp player_name do
-    Application.get_env(:handin2, :player_name)
+    Utils.get_player_name()
   end
 
   defp update_state_server_cheated(state) do
@@ -127,11 +160,47 @@ defmodule Handin2.Client do
   defp post(endpoint, body, opts) do
     {:ok, resp} = HTTPoison.post("https://" <> opts[:host] <> endpoint,
       Poison.encode!(body),
-      @headers, [hackney: [:insecure]])
+      @headers, req_opts())
     {resp, resp.body |> Poison.decode!()}
+  end
+
+  defp req_opts do
+    [
+      ssl: [
+        versions: [:"tlsv1.2"],
+        verify: :verify_peer,
+        cacertfile: Security.config(:cacert),
+        certfile: Security.config(:cert),
+        keyfile: Security.config(:privatekey),
+        ciphers: :ssl.cipher_suites(:strong, :"tlsv1.2"),
+        depth: 3,
+        customize_hostname_check: [
+          match_fun: :public_key.pkix_verify_hostname_match_fun(:https)
+        ]
+      ]
+    ]
   end
 
   defp schedule_next_game(timeout \\ 2) do
     Process.send_after(self(), :play, :timer.seconds(timeout))
+  end
+
+  defp schedule_games_if_playing() do
+    case Utils.is_playing? do
+      true ->
+        timeout = 3
+        Logger.info("Player (client) process started. Scheduling to begin playing in #{inspect(timeout)} seconds.", player: player_name())
+        schedule_next_game(timeout)
+      false -> :ok
+    end
+  end
+
+  defp gen_new_state() do
+    %{
+      wins: 0,
+      losses: 0,
+      draws: 0,
+      server_cheated: 0,
+    }
   end
 end
